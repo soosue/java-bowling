b49fe42cb63ed1fdb423206ec693cd6bb3f0757f
soosue <yunsoo0510@daum.net>
2021-12-23 17:50:59 +0900

    [feat] outin방식 5차 보완 - KnockedPinCount 상속제거

--- src/main/java/bowl2/domain/KnockedPinCount.java
+++ src/main/java/bowl2/domain/KnockedPinCount.java
@@ -1,14 +1,49 @@
 package bowl2.domain;
 
-import bowling.domain.PinCount;
-
-public class KnockedPinCount extends PinCount {
+public class KnockedPinCount {
     public static final KnockedPinCount STRIKE_COUNT = new KnockedPinCount(10);
+    public static final String INVALID_PIN_COUNT_MESSAGE = "유효하지 않은 핀의 개수입니다.";
+
+    private static final int MIN_VALUE = 0;
+    private static final int MAX_VALUE = 10;
+
+    private final int value;
 
     public KnockedPinCount(int count) {
-        super(count);
+        if (count < MIN_VALUE || count > MAX_VALUE) {
+            throw new IllegalArgumentException(INVALID_PIN_COUNT_MESSAGE);
+        }
+        value= count;
     }
-    public KnockedPinCount sum(PinCount knockOutCount) {
-        return new KnockedPinCount(value() + knockOutCount.value());
+
+    public KnockedPinCount count(KnockedPinCount knockOutCount) {
+        return new KnockedPinCount(value - knockOutCount.value);
+    }
+
+    public KnockedPinCount sum(KnockedPinCount knockOutCount) {
+        return new KnockedPinCount(value + knockOutCount.value);
+    }
+
+    public int value() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        KnockedPinCount pinCount = (KnockedPinCount) o;
+
+        return value == pinCount.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value;
     }
 }
