1dea1c7abc2f6df57dc655a439898c99c5ec5b86
soosue <yunsoo0510@daum.net>
2021-12-23 17:26:23 +0900

    [feat] outin방식 3차 보완 - abstract knockedPinCounts

--- src/main/java/bowl2/domain/FinalKnockedPinCounts.java
+++ src/main/java/bowl2/domain/FinalKnockedPinCounts.java
@@ -1,37 +1,17 @@
 package bowl2.domain;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-public class FinalKnockedPinCounts implements KnockedPinCounts {
-    public static final String WRONG_BOWL_COUNT_MESSAGE = "잘못된 투구수입니다.";
-
-    private static final int ZERO = 0;
-    private static final int ONE = 1;
-    private static final int TWO = 2;
+public class FinalKnockedPinCounts extends AbstractKnockedPinCounts {
     private static final int MAX_SIZE = 3;
-
-    private final List<KnockedPinCount> values;
 
     public FinalKnockedPinCounts() {
         this(new ArrayList<>());
     }
 
     public FinalKnockedPinCounts(List<KnockedPinCount> knockedPinCounts) {
-        if (knockedPinCounts.size() > MAX_SIZE) {
-            throw new IllegalArgumentException(WRONG_BOWL_COUNT_MESSAGE);
-        }
-        this.values = knockedPinCounts;
-    }
-
-    private void checkValidKnockedPinCounts(int count) {
-        sum(count);
-    }
-
-    private KnockedPinCount sum(int count) {
-        return values.stream()
-                .reduce(new KnockedPinCount(count), (previous, current) -> previous.sum(current));
+        super(knockedPinCounts, MAX_SIZE);
     }
 
     @Override
@@ -49,20 +29,10 @@ public class FinalKnockedPinCounts implements KnockedPinCounts {
         return isBonusEnd() || (!isDouble() && !isSpare() && isSecondEnd());
     }
 
-    @Override
-    public boolean isStrike() {
-        return isFirstEnd() && values.get(INDEX_ZERO).equals(KnockedPinCount.STRIKE_COUNT);
-    }
-
     private boolean isDouble() {
         return values.stream()
                 .filter(KnockedPinCount.STRIKE_COUNT::equals)
                 .count() == TWO;
-    }
-
-    @Override
-    public boolean isSpare() {
-        return isSecondEnd() && sum(ZERO).equals(KnockedPinCount.STRIKE_COUNT);
     }
 
     @Override
@@ -71,27 +41,7 @@ public class FinalKnockedPinCounts implements KnockedPinCounts {
     }
 
     @Override
-    public boolean isFirstEnd() {
-        return values.size() == ONE;
-    }
-
-    @Override
-    public boolean isSecondEnd() {
-        return values.size() == TWO;
-    }
-
-    @Override
     public boolean isBonusEnd() {
         return values.size() == MAX_SIZE;
-    }
-
-    @Override
-    public int getFirst() {
-        return values.get(KnockedPinCounts.INDEX_ZERO).value();
-    }
-
-    @Override
-    public List<KnockedPinCount> getValues() {
-        return Collections.unmodifiableList(values);
     }
 }
--- src/main/java/bowl2/domain/NormalKnockedPinCounts.java
+++ src/main/java/bowl2/domain/NormalKnockedPinCounts.java
@@ -1,34 +1,17 @@
 package bowl2.domain;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
-public class NormalKnockedPinCounts implements KnockedPinCounts {
-    public static final String WRONG_BOWL_COUNT_MESSAGE = "잘못된 투구수입니다.";
-
+public class NormalKnockedPinCounts extends AbstractKnockedPinCounts {
     private static final int MAX_SIZE = 2;
-
-    private final List<KnockedPinCount> values;
 
     public NormalKnockedPinCounts() {
         this(new ArrayList<>());
     }
 
     public NormalKnockedPinCounts(List<KnockedPinCount> knockedPinCounts) {
-        if (knockedPinCounts.size() > MAX_SIZE) {
-            throw new IllegalArgumentException(WRONG_BOWL_COUNT_MESSAGE);
-        }
-        values = knockedPinCounts;
-    }
-
-    private void checkValidKnockedPinCounts(int count) {
-        sum(count);
-    }
-
-    private KnockedPinCount sum(int count) {
-        return values.stream()
-                .reduce(new KnockedPinCount(count), (previous, current) -> previous.sum(current));
+        super(knockedPinCounts, MAX_SIZE);
     }
 
     @Override
@@ -43,42 +26,12 @@ public class NormalKnockedPinCounts implements KnockedPinCounts {
     }
 
     @Override
-    public boolean isStrike() {
-        return values.size() == 1 && values.get(0).equals(KnockedPinCount.STRIKE_COUNT);
-    }
-
-    @Override
-    public boolean isSpare() {
-        return isSecondEnd() && sum(0).equals(KnockedPinCount.STRIKE_COUNT);
-    }
-
-    @Override
     public boolean isFinal() {
         return false;
     }
 
     @Override
-    public boolean isFirstEnd() {
-        return values.size() == 1;
-    }
-
-    @Override
-    public boolean isSecondEnd() {
-        return values.size() == 2;
-    }
-
-    @Override
     public boolean isBonusEnd() {
         throw new IllegalArgumentException("일반 프레임은 bonus가 없습니다.");
-    }
-
-    @Override
-    public int getFirst() {
-        return values.get(KnockedPinCounts.INDEX_ZERO).value();
-    }
-
-    @Override
-    public List<KnockedPinCount> getValues() {
-        return Collections.unmodifiableList(values);
     }
 }
